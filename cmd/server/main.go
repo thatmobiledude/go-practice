58f59720de8a5ad4a710ad55b65dda412df3e6a7
thatmobiledude <samyhlee94@gmail.com>
2022-08-14 12:12:28 -0600

    added db.go file, added sqlx and pq dependencies, updated main.go to connect with postgres db.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/thatmobiledude/go-practice/internal/db"
+)
 
 // Run - is going to be responsible for the instantiation and startup of our go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to the database")
+		return err
+	}
+	// this will be removed in the future; redundant since sqlx.Connect pings on intialization
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected and pinged database")
+
 	return nil
 }
 
